13ab520f0ef26af2a605f9899ee097be19950554
plusina <przemyslaw.lusina@psignite.com>
2025-10-23 15:39:22 +0200

    fix: dodaj brakujące zamknięcie div w sekcji zoom

--- floor-plan-editor-beta/frontend/src/components/RoomCanvas.jsx
+++ floor-plan-editor-beta/frontend/src/components/RoomCanvas.jsx
@@ -568,7 +568,7 @@ const RoomCanvas = ({ activeDrawingTool, setActiveDrawingTool }) => {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7" />
               </svg>
             </button>
-          </div>
+          </div> {/* Close zoom section */}
           
           <div className="flex items-center space-x-2">
             <input
@@ -598,6 +598,6 @@ const RoomCanvas = ({ activeDrawingTool, setActiveDrawingTool }) => {
       </div>
     </div>
   )
-}
+} // Close RoomCanvas component
 
 export default RoomCanvas
